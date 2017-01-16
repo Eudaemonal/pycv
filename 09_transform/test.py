9466fbad4b1196224761990d4cf8314aa572d212
eudaemonal <eudaemonal@gmail.com>
2017-01-15 19:21:24 -0800

    update

--- 09_transform/test.py
+++ 09_transform/test.py
@@ -2,11 +2,26 @@
 # Created by Eudaemon, All Rights Reserved
 
 
+import cv2
+import numpy as np
+
+
+img = cv2.imread('shuttle.jpg')
+
+rows,cols = img.shape[:2]
+
+# Transform
+
+#M = np.float32([[1,0,100],[0,1,50]])
+#dst = cv2.warpAffine(img,M,(cols,rows))
 
 
 
+# Rotation
 
+#M = cv2.getRotationMatrix2D((cols/2,rows/2),120,1)
+#dst = cv2.warpAffine(img,M,(cols,rows))
 
-
-
-
+cv2.imshow('img',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 09_transform/test3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.5
+# Created by Eudaemon, All Rights Reserved
+
+
+import vc2
+import numpy as np
+
+
+
