24a717e2890026fd1f7ffeb716b9ea9527eeb731
eudaemonal <eudaemonal@gmail.com>
2017-01-18 20:14:26 -0800

    update

--- 22_surf/test.py
+++ 22_surf/test.py
@@ -4,14 +4,13 @@
 
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
+
 
 img = cv2.imread('../src/shuttle.jpg')
 
-surf = cv2.SURF(400)
+surf = cv2.xfeatures2d.SURF_create(400)
+kp, des = surf.detectAndCompute(img,None)
 
-
-
-
-
-
-
+img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
+plt.imshow(img2),plt.show()
